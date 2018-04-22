7486e31ec0a32b704602acc3059b3e145ecf0b88
abush <abush@PUNKOR-PC>
2018-04-22 16:22:55 +0300

    forrm app

---
+++ BinaryConverterUI/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace BinaryConverterUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a number";
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(98, 12);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(100, 20);
+            this.inputBox.TabIndex = 1;
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(98, 53);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(75, 23);
+            this.convertButton.TabIndex = 2;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(233, 103);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button convertButton;
+    }
+}
+
---
+++ BinaryConverterUI/Form1.cs
@@ -0,0 +1,43 @@
+﻿using BinaryConverterUI.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BinaryConverterUI
+{
+    public partial class Form1 : Form
+    {
+        private BinaryConverterServiceClient client;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int m = Int32.Parse(inputBox.Text);
+                string binary = client.GetBinary(m);
+                int oneCount = client.GetOneCount(m);
+                MessageBox.Show($"Binary of {m} is {binary}. It has {oneCount} of 1");
+            }
+            catch (FormatException err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new BinaryConverterServiceClient();
+        }
+    }
+}
